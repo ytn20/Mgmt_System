dbbbd47b304af23d3b80d06e65227ad925f1b03b
ytn20 <yeshatpm@gmail.com>
2023-04-28 01:37:11 -0400

    updating student repository

--- src/main/java/com/student/api/student/repository/StudentRepository.java
+++ src/main/java/com/student/api/student/repository/StudentRepository.java
@@ -1,13 +1,8 @@
 package com.student.api.student.repository;
 
+import com.student.api.student.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.domain.Student;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository {
-
-    <Student, Long>
-
-
-}
+public interface StudentRepository extends JpaRepository <Student, Long> {}
